e25c565a8cbf9e100d7a4b8e736ae2babdfa299c
Shankar Gupta <Shankargupta5522@gmail.com>
2024-02-11 14:39:53 +0100

    first commit

---
+++ DataModel.cs
@@ -0,0 +1,12 @@
+namespace ModelData
+{
+    [Serializable]
+    public class jsonData
+    {
+        public int? id { get; set; }
+        public string? nm { get; set; }
+        public string? cty { get; set; }
+        public string? hse { get; set; }
+        public string? yrs { get; set; }
+    }
+}
